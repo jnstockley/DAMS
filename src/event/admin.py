6fb8f6b4e85d8b6ff56e6b202a11a223510d6df6
Jordyn Iannuzzelli <jordyn-iannuzzelli@uiowa.edu>
2022-09-26 22:05:20 -0500

    adding event pages and files

---
+++ src/event/admin.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+
+admin_blueprint = Blueprint('admin', __name__)
+
+
+# this opens the admin main page
+@admin_blueprint.route('/admin-home')
+def adminHome():
+    return render_template("admin-home.html")
+
+
+# this opens the admin create events page
+@admin_blueprint.route('/create-event')
+def createEvent():
+    return render_template("create-event.html")
+
+
+'''
+@admin_blueprint.route('/createEvent', methods=['POST'])
+def createEvent_post():
+    #query here
+'''
