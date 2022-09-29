0df6c33bc369f95c7b7f013c7a980b1084728b3d
Jordyn Iannuzzelli <jordyn-iannuzzelli@uiowa.edu>
2022-09-28 22:48:47 -0500

    Added database commit to admin.py

--- src/event/admin.py
+++ src/event/admin.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
+from src.event.event_model import Events
+from .. import db
+from re import Pattern
 
+import re
 admin_blueprint = Blueprint('admin', __name__)
 
 
@@ -15,9 +19,22 @@ def createEvent():
     return render_template("create-event.html")
 
 
-'''
+# for getting info from boxes to send to database
 @admin_blueprint.route('/createEvent', methods=['POST'])
 def createEvent_post():
-    #query here
-    return render_template("create-event.html")
-'''
+
+    # query here, getting all textbox info from webpage
+    event_name = request.form.get("event-name")
+    town_name = request.form.get("town-name")
+    state_name = request.form.get("state-name")
+    country_name = request.form.get("country-name")
+    zip_code = request.form.get("zipcodeNum")
+    severity_level = request.form.get("severity-level")
+
+    newEvent = Events(event_name=event_name, town=town_name, state=state_name, country=country_name, zipcode=zip_code,
+                     severity_level=severity_level)
+
+    db.session.add(newEvent)
+    db.session.commit()
+
+    #STATE_REGEX = re.compile(r'[a-zA-Z]{2}')
--- src/event/event_model.py
+++ src/event/event_model.py
@@ -1,13 +1,12 @@
 from src import db
 
 
-class Event(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # primary keys are required by SQLAlchemy
+class Events(db.Model):
+    eventID = db.Column(db.Integer, primary_key=True, autoincrement=True)  # primary keys are required by SQLAlchemy
     event_name = db.Column(db.String(100))
     town = db.Column(db.String(100))
     state = db.Column(db.String(100))
     country = db.Column(db.String(100))
     zipcode = db.Column(db.Integer)
-    country_name = db.Column(db.String(3))
     severity_level = db.Column(db.String(100))
 
