23e2cfcf1fb0b1dc6ec091b5be4e355cad52e366
vinaypursnani <vinaypursnani@icloud.com>
2022-09-30 18:32:27 -0500

    replicated testing environment, installed webdriver python -m pip install flake8 pytest pip install webdriver-manager

--- src/login/login.py
+++ src/login/login.py
@@ -23,9 +23,10 @@ def verifying_user_type():
     email = request.form.get('email')
     password = request.form.get('password')
     # remember = True if request.form.get('remember') else False
-    # hashedPassword = generate_password_hash(password, method='sha256')
+    hashedPassword = generate_password_hash(password, method='sha256')
+    print(hashedPassword)
     user = User.query.filter_by(email=email).first()
-
+    print(user.account_password)
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.account_password, password):
@@ -33,32 +34,4 @@ def verifying_user_type():
         flash('Please check your login details and try again.')
         return redirect(url_for('login.login'))  # if the user doesn't exist or password is wrong, reload the page
 
-    return redirect(url_for('home.home'))
-    # email = request.form.get("email")
-    # password = request.form.get("password")
-    # print(email)
-    # print(password)
-    #
-    # hashedPassword = generate_password_hash(password, method='sha256')
-    # print(hashedPassword)
-    #
-    # account = User.query.filter_by(email=email, password=hashedPassword).first()
-    #
-    # user_exists = User.query.filter_by(email=email).first()
-    #
-    # if account:
-    #     if password == hashedPassword:
-    #         if account.admin_account == 1:
-    #             return render_template('admin_test.py')
-    #         else:
-    #             if account.verified_account == 1:
-    #                 if account.account_type == "donor":
-    #                     return render_template('donor.html')
-    #                 elif account.account_type == "recipient":
-    #                     return render_template('recipient.html')
-    #             else:
-    #                 render_template(url_for(VERIFY_ACCOUNT_PAGE))
-    #     else:
-    #         flash('Please verify your credentials!')
-    # else:
-    #     render_template(url_for(REGISTER_ACCOUNT_PAGE))
+    return redirect('home.html')
