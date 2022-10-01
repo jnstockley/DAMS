0dff150d54dc7319efa73c0aba1e8681ee940a99
vinaypursnani <vinaypursnani@icloud.com>
2022-10-01 12:24:09 -0500

    check_password_hash(user.account_password, password) uses method: str = "pbkdf2:sha256" by default to compare passwords.

--- src/login/login.py
+++ src/login/login.py
@@ -12,8 +12,6 @@ login_blueprint = Blueprint('login', __name__)
 VERIFY_ACCOUNT_PAGE = 'register.verify_account'
 REGISTER_ACCOUNT_PAGE = 'register.register'
 
-
-
 @login_blueprint.route('/login')
 def login():
     return render_template('login.html')
@@ -23,15 +21,14 @@ def verifying_user_type():
     email = request.form.get('email')
     password = request.form.get('password')
     # remember = True if request.form.get('remember') else False
-    hashedPassword = generate_password_hash(password, method='sha256')
-    print(hashedPassword)
+    print(password)
     user = User.query.filter_by(email=email).first()
     print(user.account_password)
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.account_password, password):
-        print('creds wrong')
         flash('Please check your login details and try again.')
         return redirect(url_for('login.login'))  # if the user doesn't exist or password is wrong, reload the page
+
 
     return redirect('home.html')
