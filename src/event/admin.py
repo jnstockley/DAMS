f00193f6eea3ac56dffbd02e576bd2008de5e293
Jordyn Iannuzzelli <jordyn-iannuzzelli@uiowa.edu>
2022-10-31 15:56:56 -0500

    added 3 things

--- src/event/admin.py
+++ src/event/admin.py
@@ -1,18 +1,21 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from src.event.event_model import Events
+from src.item.item_model import Items
 from .. import db
 from re import Pattern
 
 import re
+
 admin_blueprint = Blueprint('admin', __name__)
 
 admin_createEvent_page = 'admin.createEvent'
+admin_createItem_page = 'admin.createItem'
+
 
 # this opens the admin main page
 @admin_blueprint.route('/admin-home')
 def adminHome():
     return render_template("admin-home.html")
-
 
 
 ##################################################################
@@ -22,11 +25,11 @@ def adminHome():
 def modifyItem():
     return render_template("modify_item.html")
 
-#this modifies an item and changes it in database
+
+# this modifies an item and changes it in database
 @admin_blueprint.route('/modify-item', methods=['POST'])
 def modifyItem_post():
     return 0;
-
 
 
 ####################################################################
@@ -37,30 +40,38 @@ def modifyItem_post():
 def deleteItem():
     return render_template("delete_item.html")
 
+
 # delete item
 @admin_blueprint.route('/delete-item')
 def deleteItem_post():
     return 0;
 
 
-
 ###################################################################
 
-#opens the create items page
+# opens the create items page
 @admin_blueprint.route('/create-item')
 def createItem():
     return render_template("create_item.html")
 
+
 # creates an item and adds to database
 @admin_blueprint.route('/create-item', methods=['POST'])
 def createItem_post():
-    return 0;
+    item_name = request.form.get("item-name")
+    category = request.form.get("category")
+
+    newItem = Items(itemName=item_name, category=category)
+
+    db.session.add(newItem)
+    db.session.commit()
+    flash("Item Created!")
+    return render_template("create_item.html")
+
+    # return 0;
 
 
 ###################################################################
-
-
-
 
 
 # this opens the admin create events page
@@ -69,11 +80,9 @@ def createEvent():
     return render_template("create-event.html")
 
 
-
 # for getting info from boxes to send to database
 @admin_blueprint.route('/create-event', methods=['POST'])
 def createEvent_post():
-
     # query here, getting all textbox info from webpage
     event_name = request.form.get("event-name")
     town_name = request.form.get("town-name")
@@ -83,11 +92,11 @@ def createEvent_post():
     severity_level = request.form.get("severity-level")
 
     newEvent = Events(event_name=event_name, town=town_name, state=state_name, country=country_name, zipcode=zip_code,
-                     severity_level=severity_level)
+                      severity_level=severity_level)
 
     db.session.add(newEvent)
     db.session.commit()
     flash("Event Created!")
-    return redirect(url_for(admin_createEvent_page))
+    return redirect(url_for(admin_createItem_page))
 
-    #STATE_REGEX = re.compile(r'[a-zA-Z]{2}')
+    # STATE_REGEX = re.compile(r'[a-zA-Z]{2}')
--- src/item/item_model.py
+++ src/item/item_model.py
@@ -3,5 +3,5 @@ from src import db
 
 class Items(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # primary keys are required by SQLAlchemy
-    itemName = db.Column(db.String(100))
-    category = db.Column(db.String(100))
+    itemName = db.Column(db.String(45))
+    category = db.Column(db.String(45))
