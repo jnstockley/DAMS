711719722da87d4a36f5ea58febf5d5f7f4d9388
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-29 18:02:00 -0600

    Removed old models

--- src/item/item_quantity_model.py
+++
@@ -1,8 +0,0 @@
-from src import db
-
-
-class ItemQuantity(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    item_id = db.Column(db.Integer, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    request_id = db.Column(db.Integer, nullable=False)
