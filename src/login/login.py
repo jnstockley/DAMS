b8712ff848c20305b38a45408b44a6faed1590e7
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-12-06 15:46:25 -0600

    fixed login.py

--- src/login/login.py
+++ src/login/login.py
@@ -1,13 +1,15 @@
 from werkzeug.security import check_password_hash
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from src.account.user_model import User
-from src.roles.roles_helper import get_events
 from src.roles.roles_helper import get_items
-from flask_login import LoginManager
+from flask_login import LoginManager, logout_user
 
 login_blueprint = Blueprint('login', __name__)
 
 login_manager = LoginManager()
+
+login_page = 'login.html'
+verify_account = 'verify-account.html'
 
 
 @login_blueprint.route('/home')
@@ -17,7 +19,7 @@ def home():
 
 @login_blueprint.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template(login_page)
 
 
 @login_blueprint.route('/donor')
@@ -31,6 +33,8 @@ def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
+
+    print(remember)
 
     user = User.query.filter_by(email=email).first()
 
@@ -52,13 +56,13 @@ def login_post():
                 print('User account type is recipient.')
                 return redirect(url_for('recipient.add_request'))
         else:
-            return render_template('verify-account.html')
+            return render_template(verify_account)
 
-    return email, render_template('login.html')
+    return email, render_template(login_page)
 
 
 @login_blueprint.route('/verifying_credentials', methods=['POST'])
-def verifyingCredentials():
+def verifying_credentials():
     return render_template('verifying_credentials.html')
 
 
@@ -67,11 +71,15 @@ def recipient():
     return render_template('recipient.html')
 
 
+def show_events():
+    # Needs to be implemented
+    pass
+
+
 @login_blueprint.route('/login', methods=['POST'])
 def verifying_user_type():
     email = request.form.get('email')
     password = request.form.get('password')
-    # remember = True if request.form.get('remember') else False
 
     user = User.query.filter_by(email=email).first()
 
@@ -97,15 +105,15 @@ def verifying_user_type():
             else:
                 return render_template('verifying_credentials.html')
         else:
-            return render_template('verify-account.html')
+            return render_template(verify_account)
 
 
 @login_blueprint.route('/forgot_password')
 def forgot_password():
-    return render_template('verify-account')
+    return render_template(verify_account)
 
 
 @login_blueprint.route('/logout')
 def logout():
     logout_user()
-    return render_template('login.html')
+    return render_template(login_page)
